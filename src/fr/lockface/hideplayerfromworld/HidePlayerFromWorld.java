474bb8da13d3cd939602a5af611feea05ead3e10
Nicolas Went <nicolas.went@epita.fr>
2020-04-01 00:13:51 +0200

    [Archi] Create basic plugin

---
+++ src/fr/lockface/hideplayerfromworld/HidePlayerFromWorld.java
@@ -0,0 +1,17 @@
+package fr.lockface.hideplayerfromworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HidePlayerFromWorld extends JavaPlugin
+{
+	 @Override
+	 public void onEnable()
+	 {
+		 
+	 }
+	 
+	 @Override
+	 public void onDisable()
+	 {
+	 }
+}
